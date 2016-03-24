12592ff6dceb8c099bf9f474507a6d94bc4f6cb0
xavierlu <xllgms@gmail.com>
2016-03-24 18:34:02 -0400

    added UI and bricks

---
+++ Assets/Scripts/NodeManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NodeManager : MonoBehaviour {
+
+	public HashSet<Node> selectedNodes;
+	public int sum = 7;
+	RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+		selectedNodes = new HashSet<Node> ();
+	}
+
+	void Update(){
+		if (Input.touchCount == 1)
+		{
+
+			foreach (Touch touch in Input.touches)
+			{
+				if(Physics.Raycast(Camera.main.ScreenPointToRay(touch.position), out hit)){
+					selectedNodes.Add (hit.transform.gameObject.GetComponent<Node>());
+				}
+
+				switch (touch.phase)
+				{
+
+				case TouchPhase.Canceled://the touch is cancelled
+				case TouchPhase.Ended:// the touch is ended so now we can calculate the time and distance
+					int temp = 0;
+					foreach (Node node in selectedNodes) {
+						temp += node.value;
+					}
+					if (temp == sum) {
+						foreach (Node node in selectedNodes) {
+							Destroy (node.gameObject);
+						}
+						Debug.Log ("yeaaah");
+					} else
+						Debug.Log ("nooooo");
+
+					selectedNodes.Clear ();
+					temp = 0;
+					break;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+	public Canvas creditPage;
+	public Canvas settingPage;
+	public Canvas numberPage;
+	public Button numberButton;
+
+	public void LoadPlusLevel(){
+		SceneManager.LoadScene ("Level");
+	}
+
+	public void ToggleCreditPage(){
+		ToggleButton ();
+		creditPage.enabled =  creditPage.enabled ? false : true;
+	}
+
+	public void ToggleSettingPage(){
+		ToggleButton ();
+		settingPage.enabled = settingPage.enabled ? false : true;
+	}
+
+	public void ToggleNumberPage(){
+		ToggleButton ();
+		numberPage.enabled = numberPage.enabled ? false : true;
+	}
+
+	private void ToggleButton(){
+		numberButton.image.enabled = numberButton.enabled ? false : true;
+		numberButton.enabled = numberButton.enabled ? false : true;
+	}
+
+	public void FacebookShare(){
+		
+	}
+}
