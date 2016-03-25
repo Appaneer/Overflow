39aca1acbf8d5f0c71ad91da2f61f31936ab6101
xavierlu <xllgms@gmail.com>
2016-03-24 22:39:23 -0400

    added settings page

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Net;
 
 public class UIManager : MonoBehaviour {
 
@@ -9,6 +10,10 @@ public class UIManager : MonoBehaviour {
 	public Canvas settingPage;
 	public Canvas numberPage;
 	public Button numberButton;
+	public Sprite soundOnSprite;
+	public Sprite soundOffSprite;
+	private const string FACEBOOK_URL = "http://www.facebook.com/dialog/feed";
+	private const string FACEBOOK_APP_ID = "794667970397816";
 
 	public void LoadPlusLevel(){
 		SceneManager.LoadScene ("Level");
@@ -34,7 +39,38 @@ public class UIManager : MonoBehaviour {
 		numberButton.enabled = numberButton.enabled ? false : true;
 	}
 
+	public void ToggleSound(Button soundButton){
+		soundButton.image.sprite = soundButton.image.sprite.name.Equals ("sound on") ? soundOffSprite : soundOnSprite;
+	}
+
+	public static bool isHavingWiFi()
+	{
+		try
+		{
+			using (WebClient client = new WebClient())
+				using (var stream = client.OpenRead("http://www.google.com"))
+				{
+					return true;
+				}
+		}
+		catch
+		{
+			return false;
+		}
+	}
+
 	public void FacebookShare(){
 		
+	}
+
+	void ShareToFacebook (string linkParameter, string nameParameter, string captionParameter, string descriptionParameter, string pictureParameter, string redirectParameter)
+	{
+		Application.OpenURL (FACEBOOK_URL + "?app_id=" + FACEBOOK_APP_ID +
+			"&link=" + WWW.EscapeURL(linkParameter) +
+			"&name=" + WWW.EscapeURL(nameParameter) +
+			"&caption=" + WWW.EscapeURL(captionParameter) + 
+			"&description=" + WWW.EscapeURL(descriptionParameter) + 
+			"&picture=" + WWW.EscapeURL(pictureParameter) + 
+			"&redirect_uri=" + WWW.EscapeURL(redirectParameter));
 	}
 }
