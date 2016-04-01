fa03fd450bddb28f03d29ff410ec2310c4543c7a
Jimmy Shi <Jimmyshi360@gmail.com>
2016-03-31 21:14:08 -0400

    landing page animation

---
+++ Assets/Scripts/RandomRotation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotation : MonoBehaviour {
+
+
+	void Awake() {
+		transform.Rotate(0.0f, 0.0f, Random.Range(0.0f, 360.0f));
+		Color whateverColor = new Color(Random.Range(0f,1f),Random.Range(0f,1f),Random.Range(0f,1f),1f);
+
+		MeshRenderer gameObjectRenderer = GetComponent<MeshRenderer>();
+
+		Material newMaterial = new Material(Shader.Find("Standard"));
+
+		newMaterial.color = whateverColor;
+		gameObjectRenderer.material = newMaterial ;
+	}
+
+
+}
