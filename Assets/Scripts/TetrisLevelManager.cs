117fbee3b7439cff5bab922b0768aed4ba367f30
xavierlu <xllgms@gmail.com>
2016-05-21 19:13:19 -0400

    commit 3

--- Assets/Scripts/TetrisLevelManager.cs
+++ Assets/Scripts/TetrisLevelManager.cs
@@ -56,64 +56,16 @@ public class TetrisLevelManager : LevelManager {
 						min = numberOfNodesInCol [i];
 					}
 				}
-				int temp = UnityEngine.Random.Range (0, 50);
+				int temp = UnityEngine.Random.Range (0, 49);
 				GameObject hii;
 				if (temp < 48)
 					hii = Instantiate (bricks [temp % 6], spawnPoints [index].position, Quaternion.Euler (0, 180, 0)) as GameObject;
-				else if(temp == 48)
-					hii = Instantiate (bomb, spawnPoints [index].position, Quaternion.Euler (0, 180, 0)) as GameObject;
 				else
-					hii = Instantiate (coin, spawnPoints [index].position, Quaternion.Euler (0, 180, 0)) as GameObject;
+					hii = Instantiate (bomb, spawnPoints [index].position, Quaternion.Euler (0, 180, 0)) as GameObject;
 				hii.GetComponent<Node> ().col = index + 1;
 				++TetrisLevelManager.numberOfNodesInCol [index];
 			}				
 			accumulator = timeToSpawn;
-		}
-	}
-
-	public void SpawnPowerups(string powerups){
-		int temp;
-		if (index == spawnPoints.Length - 1)
-			temp = 0;
-		else
-			temp = index + 1;
-		switch (powerups) {
-		case "horizontal":
-			if (PlayerPrefs.GetInt ("NumHor") <= 0)
-				return;
-			Instantiate (horizontal, spawnPoints [temp].position, Quaternion.Euler (0, 180, 0));
-			PlayerPrefs.SetInt ("NumHor", PlayerPrefs.GetInt("NumHor") - 1);
-			UIManager.updateText (GameObject.Find("horizontal text").GetComponent<Text>(), PlayerPrefs.GetInt("NumHor"));
-			break;
-
-		case "vertical":
-			if (PlayerPrefs.GetInt ("NumVer") <= 0)
-				return;
-			Instantiate (vertical, spawnPoints [temp].position, Quaternion.Euler (0, 180, 0));
-			PlayerPrefs.SetInt ("NumVer", PlayerPrefs.GetInt("NumVer") - 1);
-			UIManager.updateText (GameObject.Find("vertical text").GetComponent<Text>(), PlayerPrefs.GetInt("NumVer"));
-			break;
-
-		case "bomb":
-			if (PlayerPrefs.GetInt ("NumBomb") <= 0)
-				return;
-			Instantiate (bomb, spawnPoints [temp].position, Quaternion.Euler (0, 180, 0));
-			PlayerPrefs.SetInt ("NumBomb", PlayerPrefs.GetInt("NumBomb") - 1);
-			UIManager.updateText (GameObject.Find("bomb text").GetComponent<Text>(), PlayerPrefs.GetInt("NumBomb"));
-			break;
-		}
-	}
-
-	public void UseNumberPowerups(int number){
-		if (PlayerPrefs.GetInt ("Num" + number) <= 0)
-			return;
-		GameObject[] arr = GameObject.FindGameObjectsWithTag ("Node");
-		PlayerPrefs.SetInt ("Num"+number, PlayerPrefs.GetInt("Num"+number) - 1);
-		UIManager.updateText (GameObject.Find(number+" text").GetComponent<Text>(), PlayerPrefs.GetInt("Num"+number));
-		foreach(GameObject i in arr){
-			Node tempNode = i.GetComponent<Node> ();
-			if (tempNode.value == number)
-				tempNode.Destroy ();
 		}
 	}
 
