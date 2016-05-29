4038fb41506b2398ce15f1b15440342e46dca0f0
xavierlu <xllgms@gmail.com>
2016-05-29 12:31:49 -0400

    Revert "Revert "restore the latest version + added paused panel""
    This reverts commit b322daa3bbe19d63112227a8714074a0b5d247ae.

---
+++ Assets/Scripts/TextColors.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class TextColors : MonoBehaviour {
+
+	Text rend;
+	float acc;
+	float timeToChange=1f;
+	void Start () {
+		rend = GetComponent<Text> ();
+		acc = timeToChange;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		acc -= Time.deltaTime;
+		if(acc<0)
+		{
+		rend.color = new Color (Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f), 1);
+			acc = timeToChange;
+		}
+	}
+}
